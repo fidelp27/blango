254873e1dcde09d846e730edd94440a1a6f6d5d5
Fidel Parabacuto <fidel2702@gmail.com>
2024-11-24 17:04:19 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -63,8 +63,8 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [BASE_DIR / "templates"],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
