90b9ae38ecd0e4d5e68c54c5635be9354a7ea1cf
Fidel Parabacuto <fidel2702@gmail.com>
2024-11-30 14:30:27 +0000

    finish custom filters

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from datetime import datetime  # Importa datetime para obtener la fecha actual
+from blog.models import Post
 # Create your views here.
 def index(request):
-  return render(request, "blog/index.html")
+  now = datetime.now()
+  posts = Post.objects.filter(published_at__lte=now)
+  return render(request, "blog/index.html", {"posts": posts})
